a53a2a68b82189b90fd6aabdb3618205bf9085d1
Srihari Ruttala <999srihari@gmail.com>
2023-06-19 00:21:41 +0530

    Added: XSS Challenges

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-tml>
+<html>
 <head>
 	<title>WBS - XSS Challenges</title>
 </head>
@@ -6,14 +6,14 @@ tml>
 	
 	<h3> WBS XSS - Challenges </h3>
 	
-	<a href="/xss/level1.php">level1</a>
-	<a href="/xss/level2.php">level2</a>
-	<a href="/xss/level3.php">level3</a>
-	<a href="/xss/level4.php">level4</a>
-	<a href="/xss/level5.php">level5</a>
-	<a href="/xss/level6.php">level6</a>
-	<a href="/xss/level7.php">level7</a>
-	<a href="/xss/level8.php">level8</a>
+	<p><a href="/xss/level1.php">level1</a></p>
+	<p><a href="/xss/level2.php">level2</a></p>
+	<p><a href="/xss/level3.php">level3</a></p>
+	<p><a href="/xss/level4.php">level4</a></p>
+	<p><a href="/xss/level5.php">level5</a></p>
+	<p><a href="/xss/level6.php">level6</a></p>
+	<p><a href="/xss/level7.php">level7</a></p>
+	<p><a href="/xss/level8.php">level8</a></p>
 
 	<p>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp <b>By &nbsp&nbspSrihari Ruttala</b></p>
 
